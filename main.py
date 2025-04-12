266dc853bf427037ac2a700699cfef23375922a6
Анзорчик <anzorchik@Noutbuk-Anzorcik.local>
2025-04-12 18:56:15 +0300

    feat: прописал CORS, дополнил документацию к апи

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from users.routers import router as user_router
 from issues.routers import router as issue_router
+from config import settings
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(user_router)
 app.include_router(issue_router)
 
--- users/routers.py
+++ users/routers.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 from typing import Annotated
 from users.models import User
+from users.exceptions import UserException
 
 from users.repositories import UserRepository
 
@@ -8,11 +9,11 @@ UserRepoDep = Annotated[UserRepository, Depends(UserRepository)]
 
 router = APIRouter(prefix='/users')
 
-@router.post('/')
+@router.post('/', responses={status.HTTP_201_CREATED: {'model': User}, status.HTTP_422_UNPROCESSABLE_ENTITY: {'model': UserException}}, status_code=status.HTTP_201_CREATED)
 def add_user_router(user: User, repo: UserRepoDep):
     user = repo.create(user)
     return user
 
-@router.get('/')
+@router.get('/', responses={status.HTTP_200_OK: {'model': User}})
 def get_users_router(repo: UserRepoDep):
     return repo.get_all()
